aa0a95afd5690272f17fb8ab4d59b14711c149d6
Maria <mararyabina62@gmail.com>
2023-02-05 01:36:40 +1000

    Добились 100% покрытия тестами

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -12,3 +12,6 @@ class Testarrs(unittest.TestCase):
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs.my_slice([]), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], -5, 2), [1, 2])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], -3, 3), [2, 3])
